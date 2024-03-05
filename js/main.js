65c0425c31282cb87c55e227be5e3870f3404cb9
roya04 <royahuseyn2004@mail.ru>
2024-03-05 12:57:58 +0400

    final

--- js/main.js
+++ js/main.js
@@ -177,3 +177,41 @@ window.addEventListener('DOMContentLoaded', function(event) {
   animateCounters();
 });
 
+//layout.js
+
+const closeBtn = document.getElementById('menuClose');
+const menuBtn = document.getElementById('menuIcon');
+const menu = document.getElementById('menu');
+const bottom_overlay=document.getElementById('bottom-overlay')
+menuBtn.addEventListener('click', function () {
+    menu.classList.add('active')
+    bottom_overlay.classList.add('active')
+
+  });
+  closeBtn.addEventListener('click', function () {
+    menu.classList.remove('active')
+    bottom_overlay.classList.remove('active')
+
+
+
+  });
+  function toggleList(listId) {
+    var list = document.getElementById(listId);
+
+    if (!list.classList.contains('open')) {
+        list.classList.add('open');
+        list.style.maxHeight = list.scrollHeight + 'px';
+    } else {
+        list.classList.remove('open');
+        list.style.maxHeight = '0';
+    }
+}
+window.addEventListener('scroll', function () {
+    var header = document.getElementById('header');
+
+    if (window.scrollY > 100) {
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+    }
+  });
